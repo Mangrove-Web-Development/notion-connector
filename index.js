493012c5917b8e3cbe077ecc211d451b42857821
aykan <aykanburcak@gmail.com>
2022-12-26 14:04:22 +0300

    Adding express middleware

--- index.js
+++ index.js
@@ -68,3 +68,12 @@ getDatabase(clientsDatabaseId)
 			)
 		});
 	})
+
+
+const express = require('express');
+const app = express();
+app.get('/database/:databaseId', (req, res) => {
+	getDatabase(req.params.databaseId).then(response => {
+		console.log(response);
+	}).catch(error => { return res.send(error) });
+})
